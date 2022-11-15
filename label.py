d3d785ba3d6acdc433637e20119a05d5099d092e
loorr <zjianfa@qq.com>
2022-11-15 23:33:10 +0800

    add: new blood

--- label.py
+++ label.py
@@ -1,6 +1,32 @@
+# !/usr/bin/env python
+# encoding: utf-8
+import argparse
+
 import numpy as np
 import pandas as pd
 import os
+
+__version__ = "2022.10.08"
+
+def version():
+    return "version:" + __version__
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-v', '--version', action='version', version=version(), help='Display version')
+parser.add_argument('-c', '--labelConfig', type=str, help='label config file path')
+parser.add_argument('-d', '--labelData', type=str,  help='label data file path')
+parser.add_argument('-o', '--labelOutData', type=str,  help='label data file path')
+parser.add_argument('-l', '--outLabelFolder', type=str,  help='out label folder')
+parser.add_argument('-i', '--outUriFolder', type=str,  help='out uri folder')
+
+args = parser.parse_args()
+if os.path.exists(args.outUriFolder) is False:
+    os.makedirs(args.outUriFolder, exist_ok=False)
+if os.path.exists(args.outLabelFolder) is False:
+    os.makedirs(args.outLabelFolder, exist_ok=False)
+
+
+
 
 class_map = {
     5: 0,
@@ -11,7 +37,12 @@ class_map = {
 }
 
 
-def transfer_label_class(data: pd.DataFrame, needTransfer=True) -> pd.DataFrame:
+def transfer_label_class(data, needTransfer=True):
+    """
+    :param data: : pd.DataFrame
+    :param needTransfer:
+    :return:  -> pd.DataFrame
+    """
     def get_class_code(x):
         categoryId = x['category_id']
         classId = x['class_id']
@@ -26,7 +57,11 @@ def transfer_label_class(data: pd.DataFrame, needTransfer=True) -> pd.DataFrame:
     return data
 
 
-def max_min_limit(data: pd.DataFrame) -> pd.DataFrame:
+def max_min_limit(data):
+    """
+    :param data: pd.DataFrame
+    :return:  -> pd.DataFrame
+    """
     def max_min(x):
         if x < 0:
             return 0
@@ -34,7 +69,6 @@ def max_min_limit(data: pd.DataFrame) -> pd.DataFrame:
             return 1
         else:
             return x
-
     data['coordinate_x'] = data['coordinate_x'].apply(max_min)
     data['coordinate_y'] = data['coordinate_y'].apply(max_min)
     data['width'] = data['width'].apply(max_min)
@@ -42,16 +76,25 @@ def max_min_limit(data: pd.DataFrame) -> pd.DataFrame:
     return data
 
 
-def save_labels(label_path, data: pd.DataFrame) -> None:
+def save_labels(label_path, data) :
+    """
+    :param label_path:
+    :param data: pd.DataFrame
+    :return: -> None
+    """
     grouped = data.groupby('md5')
-
     for name, group in grouped:
         fileName = label_path + "/{}.txt".format(name)
         out = group[['class_code', 'coordinate_x', 'coordinate_y', 'width', 'height']]
         out.to_csv(fileName, encoding='utf-8', header=False, index=False, sep=" ")
 
 
-def save_all_image_uri(folder, data: pd.DataFrame) -> None:
+def save_all_image_uri(folder, data):
+    """
+    :param folder:
+    :param data: pd.DataFrame
+    :return:  -> None
+    """
     uri = data['uri'].unique()
     with open(folder + "/uri.txt", 'w') as f:
         for item in uri:
@@ -59,7 +102,11 @@ def save_all_image_uri(folder, data: pd.DataFrame) -> None:
             f.write("\n")
 
 
-def read_label_map(config_path: str) -> dict:
+def read_label_map(config_path):
+    """
+    :param config_path: str
+    :return:  -> dict
+    """
     config = pd.read_csv(config_path, encoding='utf-8')
     result = {}
     for row in config.iterrows():
@@ -75,8 +122,15 @@ def read_label_map(config_path: str) -> dict:
 
 
 if __name__ == '__main__':
-    class_map = read_label_map("blood-cells-label-export/label_config_202210081650.csv")
-    labels = pd.read_csv("blood-label-221105/label_202211050637.csv"
+    labelConfigPath = args.labelConfig
+    labelDataPath = args.labelData
+    labelOutDataPath = args.labelOutData
+    outLabelFolder = args.outLabelFolder
+    outUriFolder = args.outUriFolder
+
+
+    class_map = read_label_map(labelConfigPath)
+    labels = pd.read_csv(labelDataPath
                          , encoding='utf-8'
                          , dtype={"class_id": str,
                                   'category_id': str,
@@ -96,16 +150,16 @@ if __name__ == '__main__':
     labels = max_min_limit(labels)
 
     # 导出处理之后的数据
-    labels.to_csv("blood-label-221105/label_out_202211050637.csv", encoding='utf-8', header=True, index=False, sep=",")
+    labels.to_csv(labelOutDataPath, encoding='utf-8', header=True, index=False, sep=",")
 
     # 生成单个标签文件
-    out_label_folder = "blood-label-221105/out-labels"
+    out_label_folder = outLabelFolder
     if os.path.exists(out_label_folder) is False:
         os.makedirs(out_label_folder, exist_ok=False)
     save_labels(out_label_folder, labels)
 
     # 生成图片uri文件
-    out_uri = "blood-label-221105/out"
+    out_uri = outUriFolder
     if os.path.exists(out_uri) is False:
         os.makedirs(out_uri, exist_ok=False)
     save_all_image_uri(out_uri, labels)
---
+++ organoid-export.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+python label.py -c organoid-label-export/organoid_config_202210091320.csv \
+-d organoid-label-export-221115/organoid_label_202211152203.csv \
+-o organoid-label-export-221115/organoid_label_out_202211152203.csv \
+-i organoid-label-export-221115/out-uri \
+-l organoid-label-export-221115/out-labels
+
+
+
