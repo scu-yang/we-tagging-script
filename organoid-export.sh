7e57444be83c38b89e9160d4abb0afda5b1cb895
loorr <zjianfa@qq.com>
2022-11-16 00:35:54 +0800

    change: path

--- organoid-export.sh
+++ organoid-export.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
+rootFolder=organoid-label-export-221115
+
 python label.py -c organoid-label-export/organoid_config_202210091320.csv \
--d organoid-label-export-221115/organoid_label_202211152203.csv \
--o organoid-label-export-221115/organoid_label_out_202211152203.csv \
--i organoid-label-export-221115/out-uri \
--l organoid-label-export-221115/out-labels
+-d ${rootFolder}/organoid_label_202211152203.csv \
+-o ${rootFolder}/organoid_label_out_202211152203.csv \
+-i ${rootFolder}/out-uri \
+-l ${rootFolder}/out-labels
 
 
-python export_images.py -o organoid-label-export-221115/out-images \
--f organoid-label-export-221115/out-uri/uri.txt
+python export_images.py -o ${rootFolder}/out-images \
+-f ${rootFolder}/out-uri/uri.txt
 
-python split-data-set.py -l organoid-label-export-221115/out-labels \
--i organoid-label-export-221115/out-images \
--o organoid-label-export-221115/data-set
+python split-data-set.py -l ${rootFolder}/out-labels \
+-i ${rootFolder}/out-images \
+-o ${rootFolder}/data-set
 
-cd organoid-label-export-221115
+cd ${rootFolder}
 zip -9 -r out.zip data-set
