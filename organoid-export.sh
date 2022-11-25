30ac0bbe410ace34e936e8834e476cb0b09535b7
loorr <zjianfa@qq.com>
2022-11-25 23:06:22 +0800

    export: organoid

--- organoid-export.sh
+++ organoid-export.sh
@@ -1,23 +1,30 @@
 #!/bin/bash
 
-rootFolder=organoid-label-export-221115
+rootFolder=organoid-label-export-221125
+labelName=or_train_and_var_202211252258
+filePofix=.csv
+fileFullName=${labelName}${filePofix}
+
+echo "rootFolder: "${rootFolder}
+echo "fileFullName: "${fileFullName}
 
 python label.py \
--c organoid-label-export/organoid_config_202210091320.csv \
--d ${rootFolder}/organoid_label_202211152203.csv \
--o ${rootFolder}/organoid_label_out_202211152203.csv \
+-c organoid-label-export-2211125/organoid_config_202210091320.csv \
+-d ${rootFolder}/${fileFullName} \
+-o ${rootFolder}/out_${fileFullName} \
 -i ${rootFolder}/out-uri \
 -l ${rootFolder}/out-labels
-
 
 python export_images.py \
 -o ${rootFolder}/out-images \
 -f ${rootFolder}/out-uri/uri.txt
 
 python split-data-set.py \
+-t 0.8 -e 0 -a 0.2 \
 -l ${rootFolder}/out-labels \
 -i ${rootFolder}/out-images \
 -o ${rootFolder}/data-set
 
-cd ${rootFolder}
-zip -9 -r out.zip data-set
+#cd ${rootFolder}
+#zip -9 -r out.zip data-set
+
